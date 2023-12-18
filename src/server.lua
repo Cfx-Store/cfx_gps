fc3fc256b235524abd63a682913526088dbed252
Thomas <40025225+thoo0224@users.noreply.github.com>
2023-12-18 09:06:33 +0100

    Add conditional check for showing radar in vehicles

--- src/server.lua
+++ src/server.lua
@@ -1,7 +1,9 @@
-RegisterNetEvent("baseevents:enteredVehicle", function()
-  TriggerClientEvent("cfx_gps:setRadarState", source, true)
-end)
+if Config.showRadarInVehicle then
+  RegisterNetEvent("baseevents:enteredVehicle", function()
+    TriggerClientEvent("cfx_gps:setRadarState", source, true)
+  end)
 
-RegisterNetEvent("baseevents:leftVehicle", function()
-  TriggerClientEvent("cfx_gps:setRadarState", source, false)
-end)
+  RegisterNetEvent("baseevents:leftVehicle", function()
+    TriggerClientEvent("cfx_gps:setRadarState", source, false)
+  end)
+end
