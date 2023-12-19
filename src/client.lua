13a7222ef5c1826f2292d3faf0139577a60d4697
Thomas <40025225+thoo0224@users.noreply.github.com>
2023-12-19 09:36:05 +0100

    Fix error when joining

--- src/client.lua
+++ src/client.lua
@@ -2,27 +2,50 @@ local function setRadarState(enabled)
   DisplayRadar(enabled)
 end
 
-local function refreshRadarState(item, count)
-  if (count ~= nil and count <= 0) or (item ~= nil and item ~= Config.gpsItem) then
-    setRadarState(false)
+local function hasGpsItem(inventory)
+  for _, item in pairs(inventory) do
+    if item.name == Config.gpsItem then
+      return true
+    end
+  end
+  return false
+end
+
+local function updateRadarState(inv)
+  local xPlayer = ESX.GetPlayerData()
+  local inventory = inv or xPlayer.inventory
+  local hasGps = hasGpsItem(inventory)
+  setRadarState(hasGps)
+end
+
+local function onInventoryChange(item, count)
+  print(item, count)
+  if item ~= Config.gpsItem then
     return
   end
 
-  local actualCount = count or ESX.SearchInventory({ Config.gpsItem }, 1)
-  setRadarState(actualCount > 0)
+  setRadarState(count > 0)
 end
 
-RegisterNetEvent("esx:playerLoaded", function() refreshRadarState() end)
-RegisterNetEvent("esx:addInventoryItem", refreshRadarState)
-RegisterNetEvent("esx:removeInventoryItem", refreshRadarState)
+RegisterNetEvent("esx:addInventoryItem", onInventoryChange)
+RegisterNetEvent("esx:removeInventoryItem", onInventoryChange)
+
+RegisterNetEvent("esx:playerLoaded", function(xPlayer)
+  local inventory = xPlayer.inventory
+  updateRadarState(inventory)
+end)
 
 RegisterNetEvent("cfx_gps:setRadarState", function(state)
   if state then
     setRadarState(true)
     return
   end
-
-  refreshRadarState()
+  updateRadarState()
 end)
 
-Citizen.CreateThread(refreshRadarState)
+Citizen.CreateThread(function()
+  while not ESX.IsPlayerLoaded() do
+    Citizen.Wait(100)
+  end
+  updateRadarState()
+end)
